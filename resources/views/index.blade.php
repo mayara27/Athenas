dd1c87883dc338c892fe7101a33b4b000a306eea
Mayara <mayarafernandesetec@gmail.com>
2021-01-13 23:32:13 -0300

    '.'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
     </head>
 
     <body class="sb-nav-fixed">
-        
+        <h1>Inicio</h1>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="index.html">Start Bootstrap</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
