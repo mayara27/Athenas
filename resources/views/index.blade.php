bb038be2f1f4fb98e23b40d23bdc77c3e519d7fb
diana <diana.iruna@gmail.com>
2021-01-13 23:08:01 -0300

    pequenas alterações

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,17 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        		<link rel="stylesheet" href="{{ url('css/estilo.css')}}">
+        <link rel="stylesheet" href="{{ url('css/estilo.css')}}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
-        <style type="text/css">
-        	
-        	@charset "UTF-8";
 
+        <style type="text/css">
+         	@charset "UTF-8";
         </style>
+
 		    <link rel="stylesheet" href="{{ url('css/estilo.css')}}">
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
@@ -45,7 +45,6 @@
             </ul>
         </nav>
 
-        
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
