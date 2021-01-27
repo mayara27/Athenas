fc92e9bf4cc7d64eb29e697e92739ba4b42b78b4
Mayara <mayarafernandesetec@gmail.com>
2021-01-26 21:33:12 -0300

    ajustes no bootstrap do menu lateral

--- resources/views/chamado/show.blade.php
+++ resources/views/chamado/show.blade.php
@@ -21,6 +21,7 @@
 
     <body class="sb-nav-fixed">
         <h1>Inicio</h1>
+
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="index.html">Athena's HelpDesk</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
@@ -44,6 +45,70 @@
                 </li>
             </ul>
         </nav>
+
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Menu</div>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Cliente
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">                                    
+                                    <a class="nav-link" href="{{ url('/cliente/show')}}">Lista clientes</a>
+                                    <a class="nav-link" href="{{ url('/cliente/create')}}">Novo Cliente</a>
+                                </nav>
+                            </div>
+
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayout" aria-expanded="false" aria-controls="collapseLayout">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Funcionário
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayout" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="{{ url('/usuario/show')}}">Lista Funcionários</a>
+                                    <a class="nav-link" href="{{ url('/usuario/create')}}">Novo Funcionário</a>
+                                </nav>
+                            </div>  
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Chamado
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="{{ url('/chamado/create')}}">Novo Chamado</a>
+                                    <a class="nav-link" href="{{ url('/chamado/show')}}">Todos Chamados</a>
+                                </nav>
+                            </div>
+
+                            <div class="sb-sidenav-menu-heading">Addons</div>
+                            <a class="nav-link" href="charts.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Charts
+                            </a>
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Tables
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Desenvolvido por:</div>
+                        Diana e Mayara
+                    </div>
+                </nav>
+            </div>
 
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
--- resources/views/usuario/create.blade.php
+++ resources/views/usuario/create.blade.php
@@ -68,12 +68,12 @@
                                 </nav>
                             </div>
 
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayout" aria-expanded="false" aria-controls="collapseLayout">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Funcionário
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                            <div class="collapse" id="collapseLayout" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="{{ url('/usuario/show')}}">Lista Funcionários</a>
                                     <a class="nav-link" href="{{ url('/usuario/create')}}">Novo Funcionário</a>
@@ -108,7 +108,6 @@
                     </div>
                 </nav>
             </div>
-       
 
             <div id="layoutSidenav_content">
                 <main>
