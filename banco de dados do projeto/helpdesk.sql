711e53fa13b1751dc8130750fa1e2ffe1bdca7be
Mayara <mayarafernandesetec@gmail.com>
2021-03-03 18:03:50 -0300

    Add banco de dados correto

--- banco de dados do projeto/helpdesk.sql
+++ banco de dados do projeto/helpdesk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Fev-2021 às 20:41
+-- Tempo de geração: 03-Mar-2021 às 22:01
 -- Versão do servidor: 10.4.16-MariaDB
 -- versão do PHP: 7.4.12
 
@@ -20,26 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `helpdesk`
 --
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `admins`
---
-
-CREATE TABLE `admins` (
-  `id_admin` int(11) NOT NULL,
-  `email` varchar(55) NOT NULL,
-  `password_admin` varchar(15) NOT NULL,
-  `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `admins`
---
-
-INSERT INTO `admins` (`id_admin`, `email`, `password_admin`, `active`) VALUES
-(1, 'admin@admin.com', '12345678', 1);
 
 -- --------------------------------------------------------
 
@@ -64,8 +44,14 @@ CREATE TABLE `chamados` (
 INSERT INTO `chamados` (`id_chamado`, `titulo`, `status`, `prioridade`, `descricao`, `deadline`, `id_cliente`) VALUES
 (7, 'teste', 'inativo', '0', 'teste', '2021-02-25', 4),
 (8, 'teste', 'inativo', '0', 'teste', '2021-02-25', 4),
-(9, 'Gabinete pegou fogo', 'ativo', '10', 'O gabinete do computador do prefeito pegou fogo enquanto ele fazia os registros dos gastos da prefeittura', '2021-02-14', 4),
-(10, 'computador com tela azul', 'ativo', '0', 'O computador da usuária apresenta tela azul após a mesma ver videos do carlinhos maia durante o expediente', '2021-02-16', 4);
+(9, 'Gabinete pegou fogo', 'inativo', '10', 'O gabinete do computador do prefeito pegou fogo enquanto ele fazia os registros dos gastos da prefeittura', '2021-02-14', 4),
+(10, 'computador com tela azul', 'inativo', '0', 'O computador da usuária apresenta tela azul após a mesma ver videos do carlinhos maia durante o expediente', '2021-02-16', 4),
+(12, 'Chamado', 'inativo', '0', 'oi', '2021-03-23', 4),
+(13, 'Erro 404', 'ativo', '0', 'oiii', '2021-03-20', 4),
+(14, 'Chamadoooo', 'ativo', '5', 'jsd', '2021-09-04', 4),
+(15, 'urgente', 'inativo', '10', NULL, '2021-03-03', 5),
+(16, 'O atrasado', 'ativo', '10', 'iiii', '2021-03-01', 5),
+(17, 'Deu ruim', 'ativo', '0', 'socorrooooo', '2021-03-23', 5);
 
 -- --------------------------------------------------------
 
@@ -87,8 +73,9 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id_cliente`, `nome_cliente`, `cpf`, `setor`, `telefone`, `email`) VALUES
-(4, 'Kassandra kkkkkkkkk', '18177728288', 'Recursos Humanos', '1231321233', 'Kaaah@teste.com'),
-(5, 'Dany Bonde', '12333212333', 'Recursos Humanos', '1233333333', 'dany@gmail.com');
+(4, 'Kassandra', '18177728288', 'Recursos Humanos', '1231321233', 'Kaaah@teste.com'),
+(5, 'Dany Bonde', '12333212333', 'Recursos Humanos', '1233333333', 'dany@gmail.com'),
+(6, NULL, NULL, 'Recursos Humanos', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -121,10 +108,11 @@ INSERT INTO `enderecos` (`id_endereco`, `Rua`, `num`, `bairro`, `cep`, `cidade`,
 (8, 'Barça', '121', 'Celnona', 11665420, 'guara', 'SP', 9),
 (9, 'RUa dos bobos', '90', 'Utero', 119928344, 'Pessoinha', 'SP', 13),
 (10, 'rua linda', '123', 'tamandaré', 11665420, 'sao paulo', 'SP', 14),
-(11, 'Rua perto do shopping', '872', 'Campo do galvão', 11998879, 'Guaratinguetá', 'SP', 15),
 (12, NULL, NULL, NULL, 11665420, NULL, NULL, 16),
 (13, 'Avenida Rio Branco', '123', 'Indaiá', 11665600, 'Caraguatatuba', 'SP', 17),
-(14, 'Rua Mathias de Albuquerque', '1234', 'Parque Balneário Poiares', 11665420, 'Caraguatatuba', 'SP', 18);
+(14, 'Rua Mathias de Albuquerque', '1234', 'Parque Balneário Poiares', 11665420, 'Caraguatatuba', 'SP', 18),
+(17, 'Rua Mathias de Albuquerque', NULL, 'Parque Balneário Poiares', 11665420, 'Caraguatatuba', 'SP', 30),
+(18, NULL, NULL, NULL, 11680000, 'Ubatuba', 'SP', 31);
 
 -- --------------------------------------------------------
 
@@ -199,10 +187,21 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(5, 'Iphone se', 'admin@admin.com', NULL, '$2y$10$oRBdpFsRqFzXzrZHH7c/feL/VX1lIItKM5RD5mI0JIk3611W5bxpW', 'admin', NULL, '2021-03-02 01:08:02', '2021-03-02 01:08:02'),
+(6, 'Mayara', 'mayara@admin.com', NULL, '$2y$10$vycIcva/di7ycT51UigO3uqTrSZUfVpUBhmBl8EqGKnRTYbDuJMky', 'admin', NULL, '2021-03-02 02:21:42', '2021-03-02 02:21:42'),
+(8, 'Mayara', 'mayara@admin2.com', NULL, '$2y$10$.YlCrZMao3lYZYMDKezn5edaxKXGmaDH/cq17qV5zpAN5edvoGwV6', 'admin', NULL, '2021-03-02 02:58:43', '2021-03-02 02:58:43'),
+(9, 'Thamy', 'thamy@operador.com', NULL, '$2y$10$i7efzhRpvD3WTDCItvwMUuPksETHbWa0kHV7RJeuQY6SFfqoqkG26', 'operador', NULL, '2021-03-02 08:13:27', '2021-03-02 08:13:27');
 
 -- --------------------------------------------------------
 
@@ -238,20 +237,15 @@ INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `cpf`, `cargo`, `email`, `
 (9, 'Iris Vezzaro', '11188827277', 'Recursos Humanos', 'iris@teste.com.br', '1232333388', 'Vezzarorh', 'user123', 'Atendente', '1298889900', 2, '2021-01-21', 'Ensino Superior Incompleto', 'Casado(a)'),
 (13, 'Jordana Santos', '12333322212', 'Recursos Humanos', 'jojo@gmail.com', '1233221123', 'jojo', 'user123', 'Supervisor', '12221121212', 0, '2021-03-05', 'Ensino Superior Incompleto', 'Solteiro(a)'),
 (14, 'Ana julia', '12322289899', 'Secretaria', 'ana@teste.com', '1233221233', 'testee', 'user123', 'Suporte', '1233223344', 1, '1989-05-18', 'Ensino Superior Completo', 'Solteiro(a)'),
-(15, 'Fabricio Toledo', '11122288899', 'Marketing', 'fabriciao@gmail.com', '1299228833', 'fabricioperfeito', 'user123', 'Supervisor', '1299889988', 0, '1988-06-07', 'Ensino Superior Completo', 'Solteiro(a)'),
 (16, 'Natalye Araujo', '48140380890', 'Recursos Humanos', 'nath@teste.com', '12992120107', NULL, 'user123', 'Atendente', '12992128899', 2, '1997-01-28', 'Ensino Superior Completo', 'Casado(a)'),
 (17, 'Mariana Guimarães', '12312389899', 'Desenvolvimento Web', 'mari@teste.com', '1222118899', 'juju@teste.com', 'user123', 'Suporte', '1288383388', 0, '1954-06-30', 'Ensino Superior Incompleto', 'Casado(a)'),
-(18, 'Joao Alberto', '12388898999', 'Desenvolvimento Web', 'joao@gmail.com', '1299888899', 'joaodev@gmail.com', 'user123', 'Suporte', '12999889989', 0, '1994-11-23', 'Ensino Superior Completo', 'Casado(a)');
+(18, 'Joao Alberto', '12388898999', 'Desenvolvimento Web', 'joao@gmail.com', '1299888899', 'joaodev@gmail.com', 'user123', 'Suporte', '12999889989', 0, '1994-11-23', 'Ensino Superior Completo', 'Casado(a)'),
+(30, 'Mayara', '12345678909', 'Atendimento', 'mayara@admin2.com', '2343349233', 'socorro', 'user123', 'admin', '2343349233', 2, '1988-04-23', 'Ensino Superior Incompleto', 'Divorciado(a)'),
+(31, 'Thamy', '12345678909', 'Atendimento', 'thamy@operador.com', '12345456565', 'a', 'user123', 'operador', '12345456565', 1, '1998-09-21', 'Ensino Superior Incompleto', 'Solteiro(a)');
 
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices para tabela `admins`
---
-ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Índices para tabela `chamados`
@@ -310,28 +304,22 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de tabela `admins`
---
-ALTER TABLE `admins`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de tabela `chamados`
 --
 ALTER TABLE `chamados`
-  MODIFY `id_chamado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_chamado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `enderecos`
 --
 ALTER TABLE `enderecos`
-  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `enderecos_clientes`
@@ -355,13 +343,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Restrições para despejos de tabelas
